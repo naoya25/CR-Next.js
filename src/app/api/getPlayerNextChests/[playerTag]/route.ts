bedf3d2d81b99ad1e216c973bb65b1271d83f4fe
naoya25 <onaoya2002@gmail.com>
2024-03-24 12:37:12 +0900

    dev

---
+++ src/app/api/getPlayerNextChests/[playerTag]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request, res: NextResponse) => {
+  const tag: string = req.url.split("/getPlayerNextChests/")[1];
+
+  try {
+    const baseUrl = process.env.NEXT_PUBLIC_CR_BASE_URL;
+    const token = process.env.NEXT_PUBLIC_CR_ACCESS_KEY;
+    const res = await fetch(`${baseUrl}/players/%23${tag}/upcomingchests`, {
+      headers: {
+        "cache-control": "max-age=60",
+        "content-type": "application/json; charset=utf-8",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(res.json);
+    if (res.ok) {
+      return NextResponse.json(
+        { message: "Success", res, tag },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: "Error", error: res.json() },
+        { status: res.status }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+};
